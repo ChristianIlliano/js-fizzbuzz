2f06d9d771757537bb556b5e5c7545161ae0d145
ChristianIlliano <grafica.illiano@gmail.com>
2024-03-27 17:25:39 +0100

    feat:css js and comment

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
  Scrivi un programma che stampi in console i numeri da 1 a 100,
  ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
  Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
- 
+
  **Prima di partire a scrivere codice poniamoci qualche domanda:**
  Come faccio a sapere se un numero è divisibile senza resto per un altro?
  Abbiamo visto qualcosa di particolare che possiamo usare?
@@ -17,3 +17,17 @@
 Consigli del giorno:
 1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
 2. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+
+SVOLGIMENTO
+
+Esecuzione dell'esercizio
+ Creare il ciclo for per i numeri da 1 a 100
+
+ Creare un if che mi soddisfi le tre condizioni stabilite
+    (i % 3 === 0)
+    (i % 5 === 0)
+    (i % 3 === 0) && (i % 5 === 0)
+
+ Aggiungere un elemento ad html con numero o stringa da mostrare
+ 
+ Applicare stile in base ai numeri e alle stringhe
--- js/script.js
+++ js/script.js
@@ -1,11 +1,63 @@
+// test
+// alert("ciao")
 
-for (let i = 0; i <= 100; i++) {
+// Seleziono il contenitore del file html
+const rowElem = document.querySelector(".row"); 
+// Log
+console.log(rowElem);
+
+// Creo il ciclo for dei numberi da 1 a 100
+for (let i = 1; i <= 100; i++) {
+    // Log
     console.log(i);
-    if (i % 3 === 0) {
+    // Creo una variabile per i background
+    let bgClass; 
+
+    // Creo il div degli item 
+    const boxElem = document.createElement("div");
+    
+    // Gestione condizioni per scritte sulla base dei numeri divisibili 
+
+    // Gestione per numeri divisibili sia per 5 che per 3
+    if ((i % 3 === 0) && (i % 5 === 0)) {   
+        // log 
+        console.log("FizzBuz");
+        // Inserisco la stinga solo nei div che soddisfano questa condizione
+        boxElem.innerHTML = `FizzBuzz`;
+        // Associo la classe creata in css per il background
+        bgClass = "fizz-buzz"
+        
+        // Gestione per numeri divisibili per 3
+    } else if (i % 3 === 0){                
+        // log 
         console.log("Fizz");
-    } else if (i % 5 === 0) {
-    console.log("Buzz");
-    } else if (i % 5 === 0 && i % 3 === 0) {
-    console.log("FizzBuzz");
-    }   
+        boxElem.innerHTML = `Fizz`;
+        bgClass = "fizz"
+
+        // Gestione per numeri divisibili per 5
+    } else if (i % 5 === 0){                
+        // log 
+        console.log("Buzz");
+        boxElem.innerHTML = `Buzz`;
+        bgClass = "buzz"
+
+        // Gestione tutti gli altri numeri
+    } else {                                
+        console.log("number")
+        // Inserisco il numero nei div che non fanno parte di nessuna condizione precedente
+        boxElem.innerHTML = i;
+        bgClass = "number"
+    }
+
+    // Creo tutti gli item nel div "row"
+    rowElem.append(boxElem);
+
+    // Aggiungo la classe "box" al div di ogni singolo item
+    boxElem.classList.add("box");
+
+    // Aggiungo la variabile "bgClass" al div di ogni singolo item per impostare il colore di background
+    boxElem.classList.add(bgClass);
+
+    // log
+    console.log(boxElem);
 }
